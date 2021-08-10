4cc582c22f6aeeec442f5d57c02ac2ce9504a171
MaxAnte <qzzik97@gmail.com>
2021-08-10 09:54:05 +0300

    Setup base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Some title</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.container {
+  max-width: 1200px;
+  padding: 0px 15px;
+  margin: 0 auto;
+}
+</style>
