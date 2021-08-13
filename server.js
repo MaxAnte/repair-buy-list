a643674fd7308727d01aec35ec2e5e0a1afcd2ff
MaxAnte <qzzik97@gmail.com>
2021-08-13 19:05:41 +0300

    build -> dist

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use("/api", require("./routes/server.routes"));
 console.log("NODE_ENV:", process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.use("/", express.static(path.join(__dirname, "client", "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
 }
 
